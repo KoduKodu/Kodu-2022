889155f59cafec405245a1a828082050d1f99f80
KoduKodu <kubrakradoslaw@gmail.com>
2022-03-01 22:07:45 +0100

    portfolio section, boilerplate to show posts on portfolio section

--- template-parts/content-portfolio.php
+++ template-parts/content-portfolio.php
@@ -8,8 +8,39 @@
  */
 
 ?>
+
 <!-- portoflio section -->
+
 <section class="portfolio main-padding">
     <h2 class="section-header">portfolio</h2>
-
+    <ul>
+        <li>
+            <div class="portfolio__single flex">
+                <img class="portfolio__single__image" src="<?php echo get_template_directory_uri(); ?>/images/gaudeamus-thumb.png" alt="" >
+                <div class="portfolio__single__about">
+                    <h3 class="portfolio__single__about__title">
+                        Gaudeamus
+                    </h3>
+                    <p class="portfolio__single__about__desc">
+                        Strona Nagrody Stowarzyszenia Wydawców Szkół Wyższych
+                    </p>
+                    <a href="#" class="portfolio__single__about__link arrow-after flex">zobacz więcej</a>
+                </div>
+            </div>
+        </li>
+        <li>
+            <div class="portfolio__single flex">
+                <img class="portfolio__single__image" src="<?php echo get_template_directory_uri(); ?>/images/gaudeamus-thumb.png" alt="" >
+                <div class="portfolio__single__about">
+                    <h3 class="portfolio__single__about__title">
+                        Gaudeamus
+                    </h3>
+                    <p class="portfolio__single__about__desc">
+                        Strona Nagrody Stowarzyszenia Wydawców Szkół Wyższych
+                    </p>
+                    <a href="#" class="portfolio__single__about__link arrow-after flex">zobacz więcej</a>
+                </div>
+            </div>
+        </li>
+    </ul>
 </section>
