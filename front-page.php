dec128da489c760f60b0d6643b0928d7ca4ced10
KoduKodu <kubrakradoslaw@gmail.com>
2022-03-03 17:04:41 +0100

    contact section

--- front-page.php
+++ front-page.php
@@ -18,6 +18,7 @@ get_header();
 <?php get_template_part('template-parts/content', 'hero');?>
 <?php get_template_part('template-parts/content', 'about');?>
 <?php get_template_part('template-parts/content', 'portfolio');?>
+<?php get_template_part('template-parts/content', 'contact');?>
 
 <?php
 get_footer();
